5119fc1a5b20cb0e72ebf7519b7915628f084d2b
virendranishad <nitvirendra@gmail.com>
2020-08-31 00:56:32 +0530

    Updates

---
+++ precache-manifest.d4aebb89616d020214981438cb6cc618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8465f1068bbf8cb7070897e7d2b1e3",
+    "url": "/tiktactoegame/index.html"
+  },
+  {
+    "revision": "1b4d826622920cd1cfdf",
+    "url": "/tiktactoegame/static/css/main.bf9a67a4.chunk.css"
+  },
+  {
+    "revision": "f9f8d87c3042599db119",
+    "url": "/tiktactoegame/static/js/2.8be078e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tiktactoegame/static/js/2.8be078e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4d826622920cd1cfdf",
+    "url": "/tiktactoegame/static/js/main.c8669172.chunk.js"
+  },
+  {
+    "revision": "f69f45a6f9108b7132e0",
+    "url": "/tiktactoegame/static/js/runtime-main.7e647c4b.js"
+  }
+]);
